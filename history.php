92b740597d94ffcc6c18dd28a46335952c4f592d
justdheja <justdheja@gmail.com>
2019-06-11 18:18:59 +0700

    :)

--- history.php
+++ history.php
@@ -76,30 +76,6 @@ include "content_history.php";
 
     <div id="right-panel" class="right-panel">
 
-        <!-- Header-->
-        <header id="header" class="header">
-
-            <div class="header-menu">
-                <div class="user-area dropdown float-right">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img class="user-avatar rounded-circle" src="images/admin.jpg" alt="User Avatar">
-                    </a>
-
-                    <div class="user-menu dropdown-menu">
-                        <a class="nav-link" href="#"><i class="fa fa-user"></i> My Profile</a>
-
-                        <a class="nav-link" href="#"><i class="fa fa-user"></i> Notifications <span class="count">13</span></a>
-
-                        <a class="nav-link" href="#"><i class="fa fa-cog"></i> Settings</a>
-
-                        <a class="nav-link" href="#"><i class="fa fa-power-off"></i> Logout</a>
-                    </div>
-                </div>
-            </div>
-
-        </header><!-- /header -->
-        <!-- Header-->
-
         <div class="breadcrumbs">
             <div class="col-sm-4">
                 <div class="page-header float-left">
